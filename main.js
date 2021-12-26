936a86e9a222b4d93127ced970da7b1219813ee1
Volodymyr <42534414+VolodymyrG27@users.noreply.github.com>
2021-12-26 15:03:26 +0200

    Add files via upload
    Deep cloning object

--- main.js
+++ main.js
@@ -1,43 +1,25 @@
 'use strict';
 
-//Class - класи це красива обгортка функції конструктора, класи в середені це тіж самі функції,
-//Якщо є елементи на сайті чи в програмі одинакові по структурі і тп. то найкраще використовувати класи(шаблон)
-
-class Rectangle {
-    constructor(height, width) { //які речі будуть включені в шаблон
-        this.height = height;
-        this.width = width;
+const persone = {
+    name: 'Alex',
+    tel: 55445454544,
+    parent: {
+        mom: 'ala',
+        dad: 'bred'
     }
-    
-    calcArea() {
-        return this.height * this.width;
-    }
-}
+};
 
-const square = new Rectangle(10, 10);
+const a = JSON.stringify(persone); // Обєкт перетворює в Json
+console.log(a);
 
-console.log(square.calcArea());
+//З сервера приходить JSON і його необхідно перетворити в звичайний об*єкт
+const b = (JSON.parse(JSON.stringify(persone)));
+console.log(b);
 
-//Принципи ООП
-/*1. Абстракція - відділення концепції від її екземпляра, КЛАС - це концепція, ЕКЗЕМПЛЯРИ вони створені на основі коцепції*/ 
-/*2. Наслідування - здатність нашого об*єкта чи класа базуватись на іншому об*єкті чи класі(головний механізм повторного використання якогось коду)*/
+//ГЛИБОКА КОПІЯ OBJECT
+const clone = JSON.parse(JSON.stringify(persone));
 
-class ColoredRectangleWidthText extends Rectangle { //Наслідуємось від попереднього класу і отримуємо в  використання його властивості і методи
-    constructor(height, width, text, bgColor) { //Аргументи всеодно потрібно передавати в конструктор
-        super(height, width); //викликає конструктор предка і він завжди має бути на першому місці в конструкторі!!! і також можемо вказувати ті властивості які хочемо використовувати
-        /*|
-        this.height = height;
-        this.width = width;
-        */
-        this.text = text;
-        this.bgColor = bgColor;
-    }
-
-    showMyProps() {
-        console.log(`TEXT: ${this.text}, color: ${this.bgColor}`);
-    }
-}
-
-const div = new ColoredRectangleWidthText(25, 10, 'Hello', 'red');
-div.showMyProps();
-console.log(div.calcArea());
+/*1. JSON.stringify(persone) = перетворить існуючий об*єкт в JSON
+  2. JSON.parse - розпарсить його назад в об*єкт
+  Таким чином створиться клон об*єкта який не буде залежати від попереднього
+  */
