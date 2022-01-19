8b922188086d81766c1bbd6a522b2555b5dd4879
Volodymyr <42534414+VolodymyrG27@users.noreply.github.com>
2022-01-19 20:39:53 +0200

    Massive methods
    Massive methods

--- main.js
+++ main.js
@@ -1,29 +1,69 @@
 'use strict';
 
-//Methdos All and Race
-const test = (time) => {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve();
-        }, time);
-    });
+const names = ['Ivan', 'Alexander', 'Anna', 'Maria', 'Iva'];
+
+//filter, також вертає новий масив
+
+const shortNames = names.filter((name) => {
+    if (name.length <= 4) {
+        return name;
+    }
+});
+
+//Method Map трансформує масив так як нам треба
+
+let answers = ['Mike', 'daADS', 'SSssssSss'];
+
+const result = answers.map((answer) => {
+    return answer.toLowerCase();
+});
+
+console.log(answers);
+
+console.log(result);
+
+//methods 1.every/2.some
+const somee = [4, 'wqe', 'sfasdasd'];
+
+//2. Бере масив, перебирає його і якщо хоть один елемент підходить по певній умові то в такому випадку верне нам true якщо не знайде то false
+
+//console.log(somee.some(item => typeof(item) === 'number'));
+const res = somee.some((item) => {
+    if (typeof(item) === 'number') {
+        return item;
+    }
+});
+
+console.log(res);
+
+//1.every якщо всі елементи підходять до нашої умови тоді він поверне true
+console.log(somee.every(item => typeof(item) === 'number'));
+
+//Methods Reduce - служить для того щоб збирати масив в одне ціле 
+const arr = [4, 5, 6, 10, 11];
+//       початкове значення 0   +  4
+//                          4   +  5  
+const reduce = arr.reduce((sum, current) => sum + current);
+console.log(reduce);
+
+const string = ['apple', 'banana', 'orange', 'egg', 'potato'];
+const resString = string.reduce((sum, current) => sum + ', ' + current); // `${sum}, ${current}
+console.log(resString);
+
+//витягнути імена з обєкта
+const obj = {
+    ivan: 'person',
+    ann: 'person',
+    dog: 'animal',
+    cat: 'animal'
 };
-/*
-test(1000).then(() => {
-    console.log('1000');
-});
-test(2000).then(() => {
-    console.log('2000');
-});*/
 
-//Метод all  служить для того щоб ми точно впевнились що всі проміси вже виконались(запити на різні сервера щоб отримати фото) і ми чекаємо всі відповіді від сервера і вже потім щось робити з ними
+//entries дозволяє взяти об*єкт і перетоврити його в матрицю(масив масивів)
 
-Promise.all([test(1000), test(2000)]).then(() => {
-    console.log('All');
-});
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'person')
+.map(item => item[0]);
 
-//Method race дії навпаки, а саме коли само перший проміс виконався тоді метод race починає працювати
+console.log(newArr);
 
-Promise.race([test(1000), test(2000)]).then(() => {
-    console.log('All');
-});
+
